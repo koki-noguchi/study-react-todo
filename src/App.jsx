ce9dd0f70b7903ec6bd3ebd15f9a8d767c46e697
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-05-31 22:01:30 +0900

    htmlを用意

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,48 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <div>
+              <li>未完了です</li>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div>
+              <li>未完了です</li>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            <div>
+              <li>完了です</li>
+              <button>戻す</button>
+            </div>
+          </li>
+          <li>
+            <div>
+              <li>完了です</li>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
