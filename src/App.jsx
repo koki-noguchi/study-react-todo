00920689c20e123d46bc4398a752f4fda9e975a0
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-06-01 20:56:34 +0900

    onClickに配列のindexを渡す・spliceを試した onClickに引数を設定したら、アロー関数風に書かないと、レンダリング時に関数が実行されてしまう。

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,12 @@ export const App = () => {
     setIncompleteTodos(newTodos);
     setInputTodo("");
   };
+  const onClickDelete = (index) => {
+    const newTodos = [...inCompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -33,13 +39,13 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {inCompleteTodos.map((todo) => {
+          {inCompleteTodos.map((todo, index) => {
             return (
               <li key={todo}>
                 <div className="list-row">
                   <p>{todo}</p>
                   <button>完了</button>
-                  <button>削除</button>
+                  <button onClick={() => onClickDelete(index)}>削除</button>
                 </div>
               </li>
             );
